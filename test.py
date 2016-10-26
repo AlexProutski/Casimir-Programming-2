3bc2dd304fe94d4c05f2c77c685255de6930cd43
James Kroll <philomathie@gmail.com>
2016-10-26 11:54:45 +0000

    Added functionality to plot circle

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print('Hello World')
 
 def circumference(r):
     pi = 3.14
     c = 2*pi*r
     return c
+
+def plotcircle(r):
+    theta = np.linspace(0,2*pi,200)
+    x = np.sin(theta)
+    y = np.cos(theta)
+    plt.plot(x,y)
+    plt.show()
 
 print(circumference(6))
